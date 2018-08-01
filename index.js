38fc32d8b33e60d8b36823697f54cbeb981f31d0
Pan Cheng <pchengeducation@hotmail.com>
2018-08-01 22:37:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ Variables defined in the test suite:
 */
 var newID = 1000000000 + oldID;
 var ageIsValid = Number.isInteger(currentAge);
-const randomNumber = Math.random*20;
+const randomNumber = Math.random()*20;
 const randomInteger = Math.floor(randomNumber);
 const randomUserID = randomInteger + 1000000001;
 
